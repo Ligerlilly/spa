23137452187a6d9c88f03b2794c2c927be92cf6d
Jason <quizathaderat@yahoo.com>
2014-10-12 14:00:54 -0700

    Added files to directory tree, spa.utils_b.js and spa.faker.js done, working on spa.model.js

--- js/spa.model.js
+++ js/spa.model.js
@@ -4,6 +4,67 @@
   regexp : true, sloppy  : true, vars     : false,
   white  : true
 */
-/*global $, spa */
+/*global TAFFY, $, spa */
 
-spa.model = (function () { return {}; } ())
+spa.model = (function () {
+  'use strict';
+  var
+    configMap = {anon_id : 'a0' },
+    stateMap = {
+      anon_user      : null,
+      people_cid_map : {},
+      people_db      : TAFFY()
+    },
+    
+    isFakeData = true,
+    personProto, makePerson, people, initModule;
+    
+    personPronto = function () {
+      get_is_user : function () {
+      	return this.cid === stateMap.user.cid;
+      },
+      get_is_anon : function () {
+      	return this.anon_id === stateMap.anon_user.cid
+      }
+    };
+    
+    makePerson = function ( person_map ) {
+      var person,
+        cid     = person_map.cid,
+        css_map = person_map.css_map,
+        id      = person_map.id,
+        name    = person_map.name
+        
+        if ( cid === undefined || ! name ) {
+        	throw 'client id and name required';
+        }
+        
+        person         = Object.create( personProto );
+        person.cid     = cid;
+        person.css_map = css_map;
+        person.name    = name;
+        
+        if ( id ) { person.id = id; }
+        
+        stateMap.people_cid_map[ cid ] = person;
+        stateMap.people_db.insert( person );
+        return person; 
+    };
+    
+    people = {
+      get_db    : function () { return stateMap.people_db },
+      get_cid_map : function () { return stateMap.people_cid_map }
+    };
+    
+    intiModule = function () {};
+
+
+
+
+
+
+
+
+
+
+}());
